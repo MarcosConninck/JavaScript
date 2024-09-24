f6afe486f71f3bfa7b8826525d4f474b26cb6990
marcos <git.conninck@gmail.com>
2024-09-24 16:20:08 -0300

    renamed:    projects/2_dataTypeOperators/DataType.js -> projects/2_data_type_operators/data_type.js 	renamed:    projects/2_dataTypeOperators/index.html -> projects/2_data_type_operators/index.html 	modified:   projects/3_programming_structures/script.js

---
+++ projects/3_programming_structures/script.js
@@ -0,0 +1,22 @@
+// let my_name = "Marcos";
+// console.log(my_name);
+// console.log(`My name is ${my_name}`);
+
+// let oranges = 5;
+// console.log(oranges * 6);
+
+// let my_name = prompt("Tell me your name");
+function salute(my_name) {
+  console.log(`Hello there, ${my_name}`);
+}
+// salute(my_name);
+// alert("get out!");
+
+let biggest_number = Math.max(1, 4, 5, 6, 10, 2, 11);
+// console.log(biggerst_number);
+
+if (biggest_number < 11) {
+  console.log(`biggest number ${biggest_number} is lower than 11`);
+} else {
+  console.log(`the number is higher or equal to 11`);
+}
