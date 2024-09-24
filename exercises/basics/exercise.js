cfb484749accc00bf4b713cc04a0d40e6a211c70
marcos <git.conninck@gmail.com>
2024-09-24 14:24:56 -0300

    new file:   exercises/2/exercise.js 	new file:   exercises/2/index.html 	new file:   exercises/basics/exercise.js 	new file:   exercises/basics/index.html 	modified:   projects/2_dataTypeOperators/DataType.js 	new file:   projects/3_programming_structures/index.html 	new file:   projects/3_programming_structures/script.js

---
+++ exercises/basics/exercise.js
@@ -0,0 +1,20 @@
+console.log(4);
+console.log(2.3);
+console.log(2 - 1);
+
+// using boolean
+console.log(4 > 2);
+console.log(4 <= 4);
+console.log(3 != 4);
+
+// logical operators
+console.log(1 + 2 >= 3 && 1 * 3 == 3);
+console.log(true || false);
+console.log(!("marcos" == "Marcos"));
+
+// returning a NaN
+console.log("marcos" * 2.2);
+
+// Create a variable called "isActive" with a boolean value of True and print it.
+var isActive = true;
+console.log(isActive);
--- projects/2_dataTypeOperators/DataType.js
+++ projects/2_dataTypeOperators/DataType.js
@@ -1,7 +1,7 @@
 console.log(typeof 13);
 console.log(13 + (4.2 * 2) / 3);
 
-/* logical operations */
+/* logical operations*/
 console.log(typeof NaN);
 
 // string
@@ -17,3 +17,31 @@ console.log(5 > 3);
 console.log(2 > 3);
 console.log(typeof (2 > 3));
 console.log();
+
+console.log("&& retornando false: ");
+console.log(true && false); // false
+console.log(5 > 3 && 3 == 2);
+
+console.log("&& retornando true: ");
+console.log(true && true); // true
+console.log("marcos" === "marcos" && 3 == 3);
+
+console.log("|| retornando false: ");
+console.log(false || false); // false
+console.log(2 == 3 || 3 == 2);
+
+console.log("!true retornando false: ");
+console.log(!true);
+
+// ternary boolean
+// condition  if    true else false
+console.log(5 > 2 ? "yes" : "no");
+
+// empty
+console.log(null);
+console.log(undefined);
+
+console.log(3 * null); // 0
+console.log(typeof ("5" - 3)); // 2
+console.log("5" + 1); // 51
+console.log("two" * "three"); //NaN
