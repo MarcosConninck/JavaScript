a31588749cdf5b186d5bb224e8f27a4415637176
marcos <git.conninck@gmail.com>
2024-09-24 10:32:05 -0300

    renamed:    projects/intro/index.html -> projects/1_intro/index.html 	new file:   projects/1_intro/script.js 	new file:   projects/2_dataTypeOperators/DataType.js 	new file:   projects/2_dataTypeOperators/index.html 	deleted:    projects/intro/script.js

---
+++ projects/2_dataTypeOperators/DataType.js
@@ -0,0 +1,19 @@
+console.log(typeof 13);
+console.log(13 + (4.2 * 2) / 3);
+
+/* logical operations */
+console.log(typeof NaN);
+
+// string
+console.log(typeof "this is a string");
+console.log(typeof `this is a string`); // template literals;
+console.log("this is \n a string in 2 lines");
+
+console.log(`this is a string ${2 + 2}`); // ` backticks;
+console.log("con" + "ca" + "te" + "na" + "te");
+
+// boolean
+console.log(5 > 3);
+console.log(2 > 3);
+console.log(typeof (2 > 3));
+console.log();
--- projects/intro/script.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello World, comming from the script")
